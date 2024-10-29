bf0963823a75a5f4623ec050f3a2bfc251a790ee
PrograMarmelad <valera_zhdanov_2000@mail.ru>
2024-10-30 01:45:44 +0500

    Lesson12

--- ExampleApp/ViewController.swift
+++ ExampleApp/ViewController.swift
@@ -8,12 +8,17 @@
 import UIKit
 
 class ViewController: UIViewController {
-private let helper = Helper()
+    
+    private let helper = Helper()
+    private let textLabel = UILabel()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .green
         updateNumber()
+        
+        setupLabel()
+        view.addSubview(textLabel)
     }
     
     private func updateNumber() {
@@ -22,5 +27,13 @@ private let helper = Helper()
         for number in helper.getNumbers(){
             print(number)
         }
+    }
+    
+    private func setupLabel() {
+        let firstNumber = helper.getNumbers().first
+        textLabel.text = firstNumber?.formatted()
+        textLabel.font = .systemFont(ofSize: 30, weight: .bold)
+        textLabel.textColor = .red
+        textLabel.frame = CGRect(x: 30, y: 30, width: 100, height: 50)
     }
 }
